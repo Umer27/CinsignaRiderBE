8e636332de3232649023a1e868b1b326cc29b84c
Umer Shaukkat <umershaukat27@gmail.com>
2021-02-26 17:39:18 +0500

    rider history

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -98,11 +98,27 @@ exports.adminTodayRecords = async(req, res) => {
 
 exports.history = async(req, res) => {
     try {
+        const timeZone = parseInt(req.query.zone)
+
+        // history will show day - 1
+        // can show any day attendances
+
+        const currentHour = moment().get('h')
+        let DAY_START
+        let DAY_END
+        if(currentHour < 6){
+            DAY_START = moment(moment().subtract(2, 'd').toDate().setHours(6 - timeZone, 0, 0, 0)).utc(true).toDate()
+            DAY_END = moment(moment().subtract(1, 'd').toDate().setHours(5 - timeZone, 59, 59, 0)).utc(true).toDate()
+        } else {
+            DAY_START = moment(moment().subtract(1, 'd').toDate().setHours(6 - timeZone, 0, 0, 0)).utc(true).toDate()
+            DAY_END = moment(moment().toDate().setHours(5 - timeZone, 59, 59, 59)).utc(true).toDate()
+        }
 
         const todayRecords = await Attendance.findAll({
             where: {
                 createdAt: {
-                    [Op.gte]: moment().subtract('1', 'day').toDate().setHours(0, 0, 0, 0)
+                    [Op.gt]: DAY_START,
+                    [Op.lt]: DAY_END
                 }
             },
             include: [ {
